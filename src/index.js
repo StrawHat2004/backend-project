c41a99b54db690f2ed593e113cac731dc59591e4
Rim Bose <rimbose123@gmail.com>
2026-01-06 20:49:40 +0530

    added Utility files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 // require('dotenv').config({path: './.env'});
 import dotenv from "dotenv";
 import connectDB from './db/index.js';
+import app from './app.js';
 
 dotenv.config(
     {
         path: './.env'
     });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on('error', (err)=>{
+        console.log("Server error:", err);
+        process.exit(1);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed:", err);
+});
